b877cd770d515d4084d35f4649ecd0fe4d6cce6c
Nishan Bista <lw.nishan@gmail.com>
2026-02-02 10:45:00 +0545

    feat: add asset path constants

---
+++ lib/core/constants/asset_paths.dart
@@ -0,0 +1,13 @@
+class AssetPaths {
+  AssetPaths._();
+  static const String _base = 'assets';
+  static const String _images = '$_base/images';
+  static const String logo = '$_images/logo.png';
+  static const String placeholder = '$_images/placeholder.png';
+  static const String avatarDefault = '$_images/avatar_default.png';
+  static const String onboarding1 = '$_images/onboarding_1.png';
+  static const String onboarding2 = '$_images/onboarding_2.png';
+  static const String onboarding3 = '$_images/onboarding_3.png';
+  static const String emptyFeed = '$_images/empty_feed.png';
+  static const String noConnection = '$_images/no_connection.png';
+}
