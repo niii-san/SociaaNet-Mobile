4737713af599b08aec0554f4dbd855c6aa084e86
Nishan Bista <lw.nishan@gmail.com>
2026-02-03 09:30:00 +0545

    feat: add date formatter utility

---
+++ lib/core/utils/date_formatter.dart
@@ -0,0 +1,18 @@
+class DateFormatter {
+  DateFormatter._();
+  static String timeAgo(DateTime date) {
+    final now = DateTime.now();
+    final diff = now.difference(date);
+    if (diff.inDays > 365) return '${diff.inDays ~/ 365}y ago';
+    if (diff.inDays > 30) return '${diff.inDays ~/ 30}mo ago';
+    if (diff.inDays > 0) return '${diff.inDays}d ago';
+    if (diff.inHours > 0) return '${diff.inHours}h ago';
+    if (diff.inMinutes > 0) return '${diff.inMinutes}m ago';
+    return 'Just now';
+  }
+  static String formatPostDate(DateTime date) {
+    final now = DateTime.now();
+    if (now.difference(date).inDays < 1) return timeAgo(date);
+    return '${date.year}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}';
+  }
+}
