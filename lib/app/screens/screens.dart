d0823aea83f111878509d1b730b87d26aa4f25cb
Nishan Bista <lw.nishan@gmail.com>
2026-02-05 16:45:00 +0545

    refactor: update screen exports

--- lib/app/screens/screens.dart
+++ lib/app/screens/screens.dart
@@ -3,12 +3,22 @@
 library;
 
 export '../../features/auth/presentation/pages/splash_screen.dart';
-export 'onboarding_screen.dart';
 export '../../features/auth/presentation/pages/login_screen.dart';
 export '../../features/auth/presentation/pages/signup_screen.dart';
-export 'main_navigation_shell.dart';
+export 'main_shell.dart';
 export 'home_feed_screen.dart';
-export 'search_screen.dart';
+export 'explore_screen.dart';
 export 'create_post_screen.dart';
-export 'dm_screen.dart';
+export 'create_reel_screen.dart';
+export 'inbox_screen.dart';
+export 'chat_screen.dart';
 export 'profile_screen.dart';
+export 'user_profile_screen.dart';
+export 'post_detail_screen.dart';
+export 'reels_screen.dart';
+export 'notifications_screen.dart';
+export 'settings_screen.dart';
+export 'activities_screen.dart';
+export 'change_password_screen.dart';
+export 'follow_requests_screen.dart';
+export 'edit_profile_screen.dart';
