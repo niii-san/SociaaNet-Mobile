1700c6c793d25ed08292d5fbee3f1a2b10c1e3a4
Nishan Bista <lw.nishan@gmail.com>
2026-02-20 11:00:00 +0545

    feat: add image cache service

---
+++ lib/core/services/cache/image_cache_service.dart
@@ -0,0 +1,23 @@
+import 'dart:collection';
+
+class ImageCacheService {
+  static final ImageCacheService _instance = ImageCacheService._internal();
+  factory ImageCacheService() => _instance;
+  ImageCacheService._internal();
+
+  final LinkedHashMap<String, dynamic> _cache = LinkedHashMap();
+  static const int _maxSize = 100;
+
+  void put(String key, dynamic value) {
+    if (_cache.length >= _maxSize) {
+      _cache.remove(_cache.keys.first);
+    }
+    _cache[key] = value;
+  }
+
+  dynamic get(String key) => _cache[key];
+
+  void clear() => _cache.clear();
+
+  bool containsKey(String key) => _cache.containsKey(key);
+}
