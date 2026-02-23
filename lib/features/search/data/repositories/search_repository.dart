e0dc7fe487565abfc180d0e6877bfbb9c7b160e9
Nishan Bista <lw.nishan@gmail.com>
2026-02-23 15:00:00 +0545

    feat: add search repository

---
+++ lib/features/search/data/repositories/search_repository.dart
@@ -0,0 +1,17 @@
+import '../datasources/search_datasource.dart';
+import '../model/search_model.dart';
+
+class SearchRepository {
+  final SearchDatasource _datasource;
+  SearchRepository(this._datasource);
+
+  Future<List<SearchResult>> searchUsers(String query) async {
+    final data = await _datasource.searchUsers(query);
+    return data.map((json) => SearchResult.fromJson(json)).toList();
+  }
+
+  Future<List<SearchResult>> searchPosts(String query) async {
+    final data = await _datasource.searchPosts(query);
+    return data.map((json) => SearchResult.fromJson(json)).toList();
+  }
+}
